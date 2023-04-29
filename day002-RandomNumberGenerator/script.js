2ef79507421ffff82b5b2e0621c4454c8117213a
ramis-khasianov <ramis.khasianov@gmail.com>
2023-04-29 22:37:35 +0500

    day 2 - Random number generator

---
+++ day002-RandomNumberGenerator/script.js
@@ -0,0 +1,12 @@
+const number = document.querySelector(".number");
+const btn = document.querySelector(".generate");
+
+const generateNumber = () => {
+    const randomNumber = Math.floor(Math.random() * 10);
+
+    number.innerHTML = randomNumber;
+}
+
+btn.addEventListener("click", generateNumber);
+
+generateNumber();
